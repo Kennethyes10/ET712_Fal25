4d05e0e549995201bb8579534ff22ae9a5e5d48b
Kennethyes10 <ksoriano251@gmail.com>
2025-09-11 15:38:45 -0400

    Create lab5_Soriano.js

---
+++ lab5_Soriano.js
@@ -0,0 +1,93 @@
+/**
+ * Kenneth Soriano
+ * lab 5, more about fuctions, object
+ * Sep 11, 2025 
+ */
+console.log("Kenneth Soriano")
+// Example 1
+// anonymous fuction in a variable
+let greet = function (username){
+    console.log(`Welcome to fuction ${username}`)
+}
+// arrow fuction
+let greeting =  (username)=>{
+    console.log(`good afternoon ${username}`)
+}
+// EXAMPLE 2: default parameters
+// randomly generated a number between 1 and 9 
+// fuction will pass the amount of time that the number will be generated
+function rand_number(x){
+for(let n = 1; n <= x; n++){
+    let num = Math.ceil (Math.random()*9)
+    console.log(`${num}`)
+}
+}
+
+// EXAMPLE 3: spread syntax ...
+numbers = [2, 0, -10, 5, 8, -9]
+let max_numbers = Math.max(numbers)
+console.log(`---Example 3 : spread -----`)
+console.log(`The maxium number is = ${max_number}`)
+
+// create an object 'car'
+const car ={
+    type:"fiat",
+    model: "500",
+    color: "white"
+}
+
+//method
+car_discription = fuction(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+breed: unknow ,
+
+// method
+meow:()=>{console.log("MEOW MEOW MEOW"){
+}
+
+// EXAMPLE 7
+const hen = {
+    // properties
+    name : "helen",
+    eggCount : 0,
+
+    // method 
+    layAnEgg : ()=>{
+        this.eggCount++
+        return `${this.name} lays an egg`
+    },
+    resetEggcount(){
+        this.eggCount = 0
+        return `${this.name} egg count = ${this.eggCount}`
+    }
+}
+
+
+// Example:8 try-catch
+
+function yell(message= ""){
+    try{
+    console.log(messgae.toUppercase().repeat(3))
+}
+catch(error){
+    console.log(error)
+    console.log('please pass a string next time!')
+}
+finally{
+    console.log('end of a program')
