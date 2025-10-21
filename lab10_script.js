a1de08e6151f339dcde3234a58ec0cb097ac6d5a
Kennethyes10 <ksoriano251@gmail.com>
2025-10-21 15:29:57 -0400

    Update lab10_script.js

--- lab10_script.js
+++ lab10_script.js
@@ -1,6 +1,6 @@
 console.log("kenneth soriano")
 // collect the elements
-let myform = document.querySelector(".myform")
+let myform = document.querySelector("#form1")
 let username = document.querySelector("#username")
 let greeting = document.querySelector("greeting")
 let displayusername = document.querySelector(".display_username")
@@ -34,6 +34,7 @@ let btnsubmit = document.querySelector(".btnsubmit")
 window.addEventListener("load", function){
     btnsubmit.disabled = true
 })
+
 let username_error_msg = document.querySelector(".username_error_msg")
 let usernameinput =document.querySelector("#username")
 usernameinput,addEventListener("input", function(){
@@ -42,9 +43,69 @@ usernameinput,addEventListener("input", function(){
     if(20>=useranamevaluelenght >=5){
         username_error_msg.textContent""
         btnsubmit.disabled = false
+        usernameinput,classlist.add("active_input_valid")
+    }
+    else if(usernamevaluelegth==20){
+        username_error_msg.textContent ="username cant be 20 characters"
     }
     else{
        username_error_msg.textcontent= "username myst  be"
        btnsubmit.disabled = true
+       usernameinput,classlist.remove("active_input_valid")
+       usernameinput,classlist.add("active_input_valid")
     }
 })
+
+/**
+ * oct 1`, 2025
+ */
+// collect the element
+const inputpassword = document.querySelector("#password")
+
+// add a focus event to the input
+inputpassword.addEventListener("focus", function(event){
+    event.classlist.add("active_input_valid")
+})
+// add a blur event to an input
+inputpassword.addEventListener("blur", function(){
+    inputpassword.classList.remove("active_input_valid")
+    inputpassword.classList.add("active_input_valid")
+})
+
+// shuffled word
+const originalword = "javascript"
+
+// function to shuffle the  original word
+function shuffleword(word){
+    return word
+    .split("")
+.sort(function()Math.random()-0.5}).join("")
+.join("")
+}
+
+
+// collect the elements
+const scrambleword= document.querySelector("#shuffledword")
+const guessinput = document.querySelector("#guessinput")
+const wordfeedback = document.querySelector("#wordfeedback")
+
+// show shuffled word
+const shuffled = shuffleword(originalword)
+
+
+// collect the elements
+const guessinput = document.querySelector("#guessinput")
+const wordfeedback = document.querySelector("wordfeedback")
+
+// add the input event
+guessinput.addEventListener("input", function(){
+    // collect characters typed in the input without begin/end space and lower case
+    const guess = guessinput.value
+
+    if(guess === originalword){
+  wordfeedback.textContent = "correct!";
+  wordfeedback.classList.add("active_input_invalid")
+    }
+    else{
+        wordfeedback.textconten = "try again";
+        wordfeedback.classList.remove("active_input_invalid")
